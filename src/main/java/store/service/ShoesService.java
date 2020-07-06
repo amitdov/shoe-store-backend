ebc643fe8108cda8f914542d32110d3f842f3e04
Amit Dov <amit.dov24@gmail.com>
2020-07-06 22:52:21 +0300

    add advanced search and start paging

--- src/main/java/store/service/ShoesService.java
+++ src/main/java/store/service/ShoesService.java
@@ -13,9 +13,14 @@ import org.springframework.stereotype.Service;
 import store.utils.SendHttpRequests;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ShoesService {
+    public static final int DEFAULT_LIMIT = 16;
+    public static final int STARTING_OFFSET = 0;
+    public static final String AND_CHARACTER = "&";
+    
     @Autowired
     private SendHttpRequests<JsonObject> sendHttpRequests;
     private static Gson gson = new GsonBuilder()
@@ -27,35 +32,31 @@ public class ShoesService {
     public List<Shoes> search(String queryText, Integer category,Integer limit, Integer offset){
 
         String request = handleSimpleRequestData(queryText, category);
-
+        request =handleLimitAndOffset(limit,offset,request);
         return getShoesByRequest(request);
     }
 
 
 
     public List<Shoes> complexSearch(String queryText, Integer category, Double startPrice, Double endPrice, String color, String brand, Integer limit, Integer offset) {
-        String request = handleSimpleRequestData(queryText, category);
-        request = HandleColorAndBrandData(color,brand, request);
-        request = HandlePriceData(startPrice, endPrice, request);
-
+        String request = handleSimpleRequestData(queryText, category,color,brand);
+        request = handlePriceData(startPrice, endPrice, request);
+        request =handleLimitAndOffset(limit,offset,request);
         return getShoesByRequest(request);
     }
 
-    private String HandleColorAndBrandData(String color, String brand, String request) {
+    private String HandleColor(String color, String request) {
         // adding the color and the brand to the string query
         if(color != null){
 //            request+="&aspect_filter=categoryId:"+category+",Color:{"+color+"}";
-            request+=" "+ color;
         }
-        if(brand != null){
-            request+=" "+brand;
-        }
+
         return request;
     }
 
-    private String HandlePriceData(Double startPrice, Double endPrice, String request) {
+    private String handlePriceData(Double startPrice, Double endPrice, String request) {
         if(startPrice != null || endPrice !=null){
-            request+="&filter=price:[";
+            request+=AND_CHARACTER +"filter=price:[";
             if(startPrice != null){
                 request+=startPrice;
             }
@@ -67,18 +68,57 @@ public class ShoesService {
         return request;
     }
 
+    private String handleSimpleRequestData(String queryText, Integer category,String color, String brand) {
+        if (category == null) {
+            category = 3034;
+        }
+        String request = apiAddress + "category_ids=" + category;
+        if(((queryText != null)&&(!queryText.isEmpty()))||
+                (color != null) ||
+                (brand != null)){
+            request += AND_CHARACTER+"q=";
+            boolean isFirst = true;
+            if (queryText != null) {
+                request +=queryText;
+                isFirst=false;
+            }
+            if(color != null){
+                if(isFirst){
+                    request+=" ";
+                }
+                request+= color;
+            }
+            if(brand != null){
+                if(isFirst){
+                    request+=" ";
+                }
+                request+=brand;
+            }
+        }
+
+        return request;
+    }
+
     private String handleSimpleRequestData(String queryText, Integer category) {
         if (category == null) {
             category = 3034;
         }
         String request = apiAddress + "category_ids=" + category;
         if (queryText != null) {
-            request += "&q=" + queryText ;
+            request += AND_CHARACTER+"q=" + queryText ;
         }
 
         return request;
     }
 
+
+
+    private String handleLimitAndOffset(Integer limit, Integer offset, String request) {
+
+        request +=AND_CHARACTER+ "limit="+ Optional.ofNullable(limit).orElse(DEFAULT_LIMIT)+
+                AND_CHARACTER +"offset="+Optional.ofNullable(offset).orElse(STARTING_OFFSET);
+        return request;
+    }
     private List<Shoes> getShoesByRequest(String request) {
         ResponseEntity<String> responseEntity = sendHttpRequests.execute(request , HttpMethod.GET, null);
 
